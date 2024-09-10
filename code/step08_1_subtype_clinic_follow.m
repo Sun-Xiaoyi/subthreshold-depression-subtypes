8734249dd166049fd2b9488cdf2891ac2f768b26
Sun-Xiaoyi <xy.xiaoyisun@gmail.com>
2024-09-10 09:54:00 +0800

    Update step08_1_subtype_clinic_follow.m

--- code/step08_1_subtype_clinic_follow.m
+++ code/step08_1_subtype_clinic_follow.m
@@ -7,4 +7,4 @@ title={'subtype','time1','time2'};
 data=table(clus_treatment,clinical_base_follow(:,5),clinical_base_follow(:,6),...
     'VariableNames',title);
 writetable(data,'D:\Data_Chen\With_DIDA_all_HC\subtype\treatment\clinical\data_forR.csv');
-%run step7_subtype_clinic_follow.R
+%run step08_subtype_clinic_follow.R
