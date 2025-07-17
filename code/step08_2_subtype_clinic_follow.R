37b5e301db0f2d7b26bea483be370f6c56c6bdf8
Sun-Xiaoyi <xy.xiaoyisun@gmail.com>
2025-07-17 17:07:42 +0800

    Update step08_2_subtype_clinic_follow.R

--- code/step08_2_subtype_clinic_follow.R
+++ code/step08_2_subtype_clinic_follow.R
@@ -1,3 +1,4 @@
+#HDRS total
 data <- read.csv("D:\\Data_Chen\\With_DIDA_all_HC\\subtype\\treatment\\clinical\\data_forR.csv")  
 data$ID <- 1:47
 library(reshape2)  
@@ -9,3 +10,19 @@ library(ez) #调用包“ez”
 result <- ezANOVA(data=data_long,dv=HDRS,wid=ID,within=.(time),
                   between=subtype,detailed=T) #重复测量方差分析
 write.csv(data.frame(result),file="D:\\Data_Chen\\With_DIDA_all_HC\\subtype\\treatment\\clinical\\result_rANOVA.csv",row.names = FALSE)
+
+#HDRS items
+library(reshape2) 
+library(ez)
+
+for (i in 1:24){
+  data <- read.csv(paste0('D:\\Data_Chen\\With_DIDA_all_HC\\subtype\\treatment\\clinical\\HDRS_item\\data_forR',i,'.csv'))
+  data$ID <- 1:47
+  data_long <- melt(data=data,id.vars=c('ID','subtype'),
+                    measure.vars=c('time1','time2'), 
+                    variable.name='time',
+                    value.name='HDRS')
+  result <- ezANOVA(data=data_long,dv=HDRS,wid=ID,within=.(time),
+                    between=subtype,detailed=T)
+  write.csv(data.frame(result),file=paste0('D:\\Data_Chen\\With_DIDA_all_HC\\subtype\\treatment\\clinical\\HDRS_item\\result_rANOVA',i,'.csv'),row.names = FALSE)
+}
